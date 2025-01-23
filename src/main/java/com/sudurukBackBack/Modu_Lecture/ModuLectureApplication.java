57d14106a3abff85ee15520fd168d006888da7b8
YSMin <zxczxv7624@gmail.com>
2025-01-24 00:12:28 +0900

    init

---
+++ src/main/java/com/sudurukBackBack/Modu_Lecture/ModuLectureApplication.java
@@ -0,0 +1,13 @@
+package com.sudurukBackBack.Modu_Lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuLectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sudurukBackBack/Modu_Lecture/ModuLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudurukBackBack.Modu_Lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
