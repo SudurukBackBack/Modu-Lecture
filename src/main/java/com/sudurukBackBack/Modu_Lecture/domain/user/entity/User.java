8fee7966d7dba1bc6f0ad3877421963b1ccbbd1a
YSMin <zxczxv7624@gmail.com>
2025-01-30 04:14:14 +0900

    [#1]feat: 회원 Entity 작성
    - UserEntity와 UserRepository 작성

--- src/main/java/com/sudurukBackBack/Modu_Lecture/domain/user/entity/User.java
+++ src/main/java/com/sudurukBackBack/Modu_Lecture/domain/user/entity/User.java
@@ -1,4 +1,75 @@
 package com.sudurukBackBack.Modu_Lecture.domain.user.entity;
 
-public class User {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicUpdate
+@Table(name = "user", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+})
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Email
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotNull
+    @Column(nullable = false)
+    private String password;
+
+    private String username; // nickname
+
+    @NotNull
+    @Column(nullable = false)
+    private Integer grade; // 0: Bronze, 1: Sliver, 2: Gold, 3: Platinum
+
+    @NotNull
+    @Column(nullable = false)
+    private Integer userStatus; // 0: INACTIVE, 1: ACTIVE
+
+    @NotNull
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(Integer.toString(grade)));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
--- src/main/java/com/sudurukBackBack/Modu_Lecture/domain/user/repository/UserRepository.java
+++ src/main/java/com/sudurukBackBack/Modu_Lecture/domain/user/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.sudurukBackBack.Modu_Lecture.domain.user.repository;
 
-public interface UserRepository {
+import com.sudurukBackBack.Modu_Lecture.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
