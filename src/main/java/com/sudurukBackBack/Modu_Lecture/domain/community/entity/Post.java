b66a2bfbc06e8e119fcd811a8e6e34b2a81afba4
jessi87 <wairi.developeteam.jh@gmail.com>
2025-02-02 10:27:16 +0900

    [#9]feat: Post Entity 작성

--- src/main/java/com/sudurukBackBack/Modu_Lecture/domain/community/entity/Post.java
+++ src/main/java/com/sudurukBackBack/Modu_Lecture/domain/community/entity/Post.java
@@ -1,4 +1,43 @@
 package com.sudurukBackBack.Modu_Lecture.domain.community.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "post")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column()
+    private String category;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column()
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private boolean isActive;
+    
 }
--- src/main/java/com/sudurukBackBack/Modu_Lecture/domain/community/repository/PostRepository.java
+++ src/main/java/com/sudurukBackBack/Modu_Lecture/domain/community/repository/PostRepository.java
@@ -1,4 +1,7 @@
 package com.sudurukBackBack.Modu_Lecture.domain.community.repository;
 
-public interface PostRepository {
+import com.sudurukBackBack.Modu_Lecture.domain.community.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
