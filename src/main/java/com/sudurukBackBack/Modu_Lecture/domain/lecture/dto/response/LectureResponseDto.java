db6db88fbf936f0fe6d95011f6d4bc508f55d654
Rio Kim <turtlekim322@gmail.com>
2025-02-17 15:34:27 +0900

    [#24]fix :기존 class → record 타입으로 변경하여 불변 객체로 관리.

--- src/main/java/com/sudurukBackBack/Modu_Lecture/domain/lecture/dto/response/LectureResponseDto.java
+++ src/main/java/com/sudurukBackBack/Modu_Lecture/domain/lecture/dto/response/LectureResponseDto.java
@@ -5,25 +5,26 @@ import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
-public class LectureResponseDto {
-    private final Long lectureId;
-    private final Long userId;
-    private final String title;
-    private final String description;
-    private final String instructor;
-    private final int category;
-    private final int price;
-    private final LocalDateTime createdAt;
-
+public record LectureResponseDto(
+        Long lectureId,
+        Long userId,
+        String title,
+        String description,
+        String instructor,
+        int category,
+        int price,
+        LocalDateTime createdAt
+) {
     public LectureResponseDto(Lecture lecture) {
-        this.lectureId = lecture.getLectureId();
-        this.userId = lecture.getUserId();
-        this.title = lecture.getTitle();
-        this.description = lecture.getDescription();
-        this.instructor = lecture.getInstructor();
-        this.category = lecture.getCategory();
-        this.price = lecture.getPrice();
-        this.createdAt = lecture.getCreatedAt();
+        this(
+                lecture.getLectureId(),
+                lecture.getUserId(),
+                lecture.getTitle(),
+                lecture.getDescription(),
+                lecture.getInstructor(),
+                lecture.getCategory(),
+                lecture.getPrice(),
+                lecture.getCreatedAt()
+        );
     }
 }
