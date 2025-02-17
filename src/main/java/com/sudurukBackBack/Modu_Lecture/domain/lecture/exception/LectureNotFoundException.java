a105d65f83914ae12526173368d7f5e92e4e8158
Rio Kim <turtlekim322@gmail.com>
2025-02-17 15:48:09 +0900

    [#24]fix : @RequiredArgsConstructor 추가하여 생성자 자동 주입

--- src/main/java/com/sudurukBackBack/Modu_Lecture/domain/lecture/exception/LectureNotFoundException.java
+++ src/main/java/com/sudurukBackBack/Modu_Lecture/domain/lecture/exception/LectureNotFoundException.java
@@ -1,7 +1,16 @@
 package com.sudurukBackBack.Modu_Lecture.domain.lecture.exception;
 
-public class LectureNotFoundException extends RuntimeException {
-    public LectureNotFoundException(String message) {
-        super(message);
+import org.springframework.http.HttpStatus;
+
+public final class LectureNotFoundException extends BasicException {
+
+    @Override
+    public int statusCode() {
+        return HttpStatus.NOT_FOUND.value();
+    }
+
+    @Override
+    public String errorMessage() {
+        return "강의를 찾을 수 없습니다.";
     }
 }
