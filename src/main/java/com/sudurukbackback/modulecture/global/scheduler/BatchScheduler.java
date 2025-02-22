c5f8d805aa8bf29fef4dcd193eb4375b62ac03f7
YSMin <zxczxv7624@gmail.com>
2025-02-22 23:24:23 +0900

    [#54]fix: 멘토링 피드백 반영
    - Batch 기능 비활성화

--- src/main/java/com/sudurukbackback/modulecture/global/scheduler/BatchScheduler.java
+++ src/main/java/com/sudurukbackback/modulecture/global/scheduler/BatchScheduler.java
@@ -2,9 +2,6 @@ package com.sudurukbackback.modulecture.global.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -20,17 +17,19 @@ public class BatchScheduler {
 
     @Scheduled(cron = "0 0 0 * * *") // 매일 자정 실행
     public void runDeletePendingUsersJob() {
-        try {
-            // Job을 동적으로 가져옴 (Bean 로딩 문제 방지)
-            Job deletePendingUsersJob = applicationContext.getBean("deletePendingUsersJob", Job.class);
+        // TODO: 추후 여력이 있을 때 Batch 구성
 
-            JobParameters jobParameters = new JobParametersBuilder()
-                    .addLong("time", System.currentTimeMillis())
-                    .toJobParameters();
-
-            jobLauncher.run(deletePendingUsersJob, jobParameters);
-        } catch (Exception e) {
-            log.error("Error running batch job", e);
-        }
+//        try {
+//            // Job을 동적으로 가져옴 (Bean 로딩 문제 방지)
+//            Job deletePendingUsersJob = applicationContext.getBean("deletePendingUsersJob", Job.class);
+//
+//            JobParameters jobParameters = new JobParametersBuilder()
+//                    .addLong("time", System.currentTimeMillis())
+//                    .toJobParameters();
+//
+//            jobLauncher.run(deletePendingUsersJob, jobParameters);
+//        } catch (Exception e) {
+//            log.error("Error running batch job", e);
+//        }
     }
 }
