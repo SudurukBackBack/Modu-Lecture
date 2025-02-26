a854d115334b094f13e87ac61379276a7e61bbe7
YSMin <zxczxv7624@gmail.com>
2025-02-27 00:59:24 +0900

    [#39]feat: 회원가입 페이지 구성
    - 회원가입 페이지 구성
    - 엔드포인트 `/web` 추가

--- src/main/java/com/sudurukbackback/modulecture/view/AuthWebController.java
+++ src/main/java/com/sudurukbackback/modulecture/view/AuthWebController.java
@@ -3,7 +3,9 @@ package com.sudurukbackback.modulecture.view;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/web")
 @Controller
 public class AuthWebController {
 
