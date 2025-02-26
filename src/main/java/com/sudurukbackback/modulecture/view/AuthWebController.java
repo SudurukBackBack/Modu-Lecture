8eebbaf264e903dc6f4c3da9645e744f792432a8
YSMin <zxczxv7624@gmail.com>
2025-02-27 00:19:29 +0900

    [#39]feat: 로그인 페이지 구성
    - 로그인 화면 생성
    - 왼쪽 상단에 위치한 아이콘을 클릭하면 메인화면으로 이동하도록 설정

---
+++ src/main/java/com/sudurukbackback/modulecture/view/AuthWebController.java
@@ -0,0 +1,20 @@
+package com.sudurukbackback.modulecture.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthWebController {
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "/domain/auth/login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        return "/domain/auth/register";
+    }
+
+}
