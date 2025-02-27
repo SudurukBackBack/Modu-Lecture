92317b62d3d0c64a047201648c5694096ee9b205
itzelic-code <en3873@gmail.com>
2025-02-27 22:01:15 +0900

    [#51] feat: mypage 화면

--- src/main/java/com/sudurukbackback/modulecture/view/MainController.java
+++ src/main/java/com/sudurukbackback/modulecture/view/MainController.java
@@ -8,9 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/main")
-    public String index(Model model) {
+    public String main(Model model) {
         model.addAttribute("pageTitle", "메인 페이지");
         return "domain/main";
+    }
+
+    @GetMapping("/mypage")
+    public String mypage(Model model) {
+        return "domain/mypage/mypage";
+    }
+
+    @GetMapping("/mypage-tutor-upload")
+    public String mypageTutorUplaod(Model model) {
+        return "domain/mypage/mypage-tutor-upload";
     }
 
 }
