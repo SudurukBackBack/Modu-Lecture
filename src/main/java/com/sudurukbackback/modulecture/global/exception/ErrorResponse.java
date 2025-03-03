0c662b180a83e6731f50a5d06080085ed3c6e961
YSMin <zxczxv7624@gmail.com>
2025-03-03 21:36:43 +0900

    [#85]fix: 오류메세지 출력
    - 검증단계에서 발생한 예외메세지도 출력하도록 수정

--- src/main/java/com/sudurukbackback/modulecture/global/exception/ErrorResponse.java
+++ src/main/java/com/sudurukbackback/modulecture/global/exception/ErrorResponse.java
@@ -3,11 +3,14 @@ package com.sudurukbackback.modulecture.global.exception;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 @Builder
 public class ErrorResponse {
 
     private int statusCode;
     private String errorMessage;
+    private final Map<String, String> validationErrors;
 
 }
