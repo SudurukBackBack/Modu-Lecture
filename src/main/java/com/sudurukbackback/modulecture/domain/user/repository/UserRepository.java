9ce8039cceb4248d4e0f8223677c8572d6f2af50
YSMin <zxczxv7624@gmail.com>
2025-03-05 17:59:36 +0900

    [#87]feat: Batch scheduler 도입
    - 탈퇴 유예 기간이 지난 계정들을 주기적으로 최종 탈퇴처리
    - 매 시간마다 작동하도록 설정

--- src/main/java/com/sudurukbackback/modulecture/domain/user/repository/UserRepository.java
+++ src/main/java/com/sudurukbackback/modulecture/domain/user/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.sudurukbackback.modulecture.domain.user.repository;
 
 import com.sudurukbackback.modulecture.domain.user.entity.User;
+import com.sudurukbackback.modulecture.domain.user.entity.enums.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +17,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     boolean existsByNickname(String nickname);
+
+    List<User> findAllByUserStatusAndDeletedAtBefore(UserStatus userStatus, LocalDateTime deleteAt);
 }
