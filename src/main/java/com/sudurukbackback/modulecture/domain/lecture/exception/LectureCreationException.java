88148abbdd0543211ff214096c3468f24372d084
YSMin <zxczxv7624@gmail.com>
2025-03-05 20:18:53 +0900

    [#91]fix: 코드 스타일 통일
    - exception 코드 통일

--- src/main/java/com/sudurukbackback/modulecture/domain/lecture/exception/LectureCreationException.java
+++ src/main/java/com/sudurukbackback/modulecture/domain/lecture/exception/LectureCreationException.java
@@ -1,7 +1,17 @@
 package com.sudurukbackback.modulecture.domain.lecture.exception;
 
-public class LectureCreationException extends RuntimeException {
-    public LectureCreationException(String message) {
-        super(message);
+import com.sudurukbackback.modulecture.global.exception.BasicException;
+import org.springframework.http.HttpStatus;
+
+public class LectureCreationException extends BasicException {
+
+    @Override
+    public int statusCode() {
+        return HttpStatus.INTERNAL_SERVER_ERROR.value();
+    }
+
+    @Override
+    public String errorMessage() {
+        return "강의 생성에 오류가 발생했습니다.";
     }
 }
