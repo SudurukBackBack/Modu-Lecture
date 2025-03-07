d9336068367cb5f6065c75a1d217abbaafb45280
itzelic-code <en3873@gmail.com>
2025-03-07 18:32:14 +0900

    fix: 수강목록 조회 api repository 오류 해결
    EnrollRepository에 쿼리 보완

--- src/main/java/com/sudurukbackback/modulecture/domain/enrollment/repository/EnrollmentRepository.java
+++ src/main/java/com/sudurukbackback/modulecture/domain/enrollment/repository/EnrollmentRepository.java
@@ -4,6 +4,8 @@ import com.sudurukbackback.modulecture.domain.enrollment.entity.Enrollment;
 import com.sudurukbackback.modulecture.domain.lecture.entity.Lecture;
 import com.sudurukbackback.modulecture.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +15,6 @@ public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
 
     boolean existsByUserAndLecture(User user, Lecture lecture);
 
-    List<Long> findLectureIdsByUserId(Long userId);
+    @Query("SELECT e.lecture.id FROM Enrollment e WHERE e.user.id = :userId")
+    List<Long> findLectureIdsByUserId(@Param("userId") Long userId);
 }
--- src/main/java/com/sudurukbackback/modulecture/domain/enrollment/service/EnrollmentService.java
+++ src/main/java/com/sudurukbackback/modulecture/domain/enrollment/service/EnrollmentService.java
@@ -11,12 +11,14 @@ import com.sudurukbackback.modulecture.domain.user.entity.User;
 import com.sudurukbackback.modulecture.domain.user.exception.UserNotExistException;
 import com.sudurukbackback.modulecture.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class EnrollmentService {
@@ -45,7 +47,7 @@ public class EnrollmentService {
         User user = userRepository.findByEmail(auth.getName())
                 .orElseThrow(UserNotExistException::new);
 
-        // 사용자가 수강 중인 강의 ID 목록 가져오기
+        // 사용자가 수강 중인 강의 ID만 있는 목록 가져오기
         List<Long> lectureIds = enrollmentRepository.findLectureIdsByUserId(user.getId());
 
         // 강의 정보를 일괄로 조회
@@ -53,6 +55,8 @@ public class EnrollmentService {
 
         // 조회한 강의 갯수가 맞지 않는 경우: 예외처리
         if (lectures.size() != lectureIds.size()) {
+            log.info("lectureIds: " + lectureIds);
+            log.info("lectures: " + lectures);
             throw new LectureNotFoundException();
         }
 
