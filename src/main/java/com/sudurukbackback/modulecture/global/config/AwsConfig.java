6266f7d2f1a01574ea6ecd395cb6b43fc8c8a158
itzelic-code <en3873@gmail.com>
2025-03-08 16:41:23 +0900

    [#37] S3 업로드용 Presigned Url

--- src/main/java/com/sudurukbackback/modulecture/global/config/AwsConfig.java
+++ src/main/java/com/sudurukbackback/modulecture/global/config/AwsConfig.java
@@ -4,9 +4,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+
+import java.time.Duration;
 
 @Configuration
 public class AwsConfig {
@@ -20,12 +26,34 @@ public class AwsConfig {
     @Value("${cloud.aws.region}")
     private String region;
 
-    @Bean
-    public S3Client s3Client() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
-        return S3Client.builder()
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+    @Value("cloud.aws.s3.bucket-name")
+    private String bucketName;
+
+    private final S3Presigner s3Presigner;
+
+    // 생성자에서 S3Presigner 객체 초기화
+    public AwsConfig() {
+        this.s3Presigner = S3Presigner.builder()
                 .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
+
+    /**
+     * 📌 업로드용 Presigned URL 생성
+     */
+    public String generateUploadPresignedUrl(String objectKey) {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectKey)
+                .build();
+
+        PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(r -> r
+                .signatureDuration(Duration.ofMinutes(30))  // URL 유효 시간 설정 (10분)
+                .putObjectRequest(putObjectRequest)
+        );
+
+        return presignedRequest.url().toString();
+    }
+
 }
