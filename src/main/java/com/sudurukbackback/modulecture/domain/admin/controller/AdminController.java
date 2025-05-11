b8bcaef4d0e09d5fd983315aeb4ef7005141e029
YSMin <zxczxv7624@gmail.com>
2025-05-11 18:49:50 +0900

    [#117]feat: 모든 사용자 정보 조회
    - 키워드 검색 가능
    - 특정 조건을 사용한 정렬 가능
    - 유효하지 않은 토큰 또는 관리자 권한이 없는 토큰 사용 시 접근 차단

--- src/main/java/com/sudurukbackback/modulecture/domain/admin/controller/AdminController.java
+++ src/main/java/com/sudurukbackback/modulecture/domain/admin/controller/AdminController.java
@@ -1,14 +1,15 @@
 package com.sudurukbackback.modulecture.domain.admin.controller;
 
 import com.sudurukbackback.modulecture.domain.admin.dto.request.AdminRegisterDto;
+import com.sudurukbackback.modulecture.domain.admin.dto.response.UserListDto;
 import com.sudurukbackback.modulecture.domain.admin.service.AdminService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admin")
@@ -32,5 +33,15 @@ public class AdminController {
         return ResponseEntity.ok("Admin Registration Success");
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/user-list")
+    public ResponseEntity<Page<UserListDto>> getUserList(
+            @RequestParam(value = "keyword", required = false) String keyword,
+            final Pageable pageable
+    ) {
+        Page<UserListDto> userList = adminService.getUserList(keyword, pageable);
+
+        return ResponseEntity.ok().body(userList);
+    }
 }
 
--- src/main/java/com/sudurukbackback/modulecture/domain/admin/service/AdminService.java
+++ src/main/java/com/sudurukbackback/modulecture/domain/admin/service/AdminService.java
@@ -1,6 +1,7 @@
 package com.sudurukbackback.modulecture.domain.admin.service;
 
 import com.sudurukbackback.modulecture.domain.admin.dto.request.AdminRegisterDto;
+import com.sudurukbackback.modulecture.domain.admin.dto.response.UserListDto;
 import com.sudurukbackback.modulecture.domain.admin.exception.AdminRegisterException;
 import com.sudurukbackback.modulecture.domain.auth.component.AuthComponent;
 import com.sudurukbackback.modulecture.domain.user.component.UserComponent;
@@ -9,6 +10,8 @@ import com.sudurukbackback.modulecture.domain.user.entity.enums.UserGrade;
 import com.sudurukbackback.modulecture.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -67,10 +70,45 @@ public class AdminService {
 
         userRepository.save(admin);
 
+        // 사용한 Code 폐기
         redisTemplate.delete(ADMIN_CODE_PREFIX + code);
     }
 
+    /**
+     * 제공된 검색 키워드를 기반으로 사용자의 페이징된 목록을 검색합니다.
+     * 키워드가 제공되면 닉네임 또는 이메일로 대소문자를 구분하지 않고 사용자를 검색합니다.
+     * 그렇지 않으면 모든 사용자를 검색합니다.
+     *
+     * @param keyword 닉네임 또는 이메일로 사용자를 필터링하는 데 사용되는 검색 키워드입니다. null 또는 비어 있을 수 있습니다.
+     * @param pageable 페이징 및 정렬 정보
+     * @return 페이징된 사용자 목록 (UserListDto로 매핑됨)
+     */
+    public Page<UserListDto> getUserList(String keyword, Pageable pageable) {
+        // 사용자 엔티티를 담는 Page 객체
+        Page<User> userPage;
 
+        // 검색 키워드가 null이 아니고 비어 있지 않은 경우 (검색 조건이 있는 경우)
+        if (keyword != null && !keyword.isEmpty()) {
+            // 닉네임 또는 이메일에 검색 키워드를 포함하는 경우
+            // Pageable 정보에 따라 페이징하여 조회
+            // ignoreCase 옵션을 사용하여 대소문자 구분 없이 검색
+            userPage = userRepository.findByNicknameContainingIgnoreCaseOrEmailContainingIgnoreCase(keyword, keyword, pageable);
+        } else {
+            // 검색 키워드가 없는 경우 (검색 조건이 없는 경우) -> 모두 조회
+            userPage = userRepository.findAll(pageable);
+        }
+
+        // 조회된 Page<User> 객체의 각 User 엔티티를 UserListDto::of 메서드를 사용하여 UserListDto 객체로 변환
+        return userPage.map(UserListDto::of);
+    }
+
+    /**
+     * Redis 캐시 내에서 제공된 관리자 등록 코드의 존재 여부와 상태 값을 확인하여 유효성을 검증합니다.
+     * 코드가 유효하지 않거나 필수 조건을 충족하지 못하면 예외를 발생시킵니다.
+     *
+     * @param code 검증할 관리자 등록 코드
+     * @throws AdminRegisterException 코드가 유효하지 않거나 캐시에서 찾을 수 없는 경우
+     */
     private void checkAdminCode(String code) {
         String storedCode = redisTemplate.opsForValue().get(ADMIN_CODE_PREFIX + code);
 
